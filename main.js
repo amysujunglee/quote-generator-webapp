05c83b085f4fdac446d09863d327d22a4973b1aa
amysujunglee <amy.sujunglee@gmail.com>
2020-01-14 23:11:43 -0800

    creating a table to store each data

--- main.js
+++ main.js
@@ -2,16 +2,22 @@ $(document).ready(function() {
   $("#btn-search").click(function() {
     $.getJSON("quotes.json", function(data) {
       var quotes = data;
+      var quotesList = document.getElementById("quotes-list");
+      var quoteBox = document.getElementById("quote-box");
+      var authorBox = document.getElementById("author-box");
+      var catBox = document.getElementById("category-box");
       var quoteOutput = "";
 
-      for (key in quotes) {
-        // console.log(response[key].quote);
-        if (quotes.hasOwnProperty(key)) {
-          quoteOutput += "<p>" + quotes[key].quote + "</p>" + "<hr />";
-        } // hasOwnProperty check
-      } // for each object
-      var quoteList = document.getElementById("quote-box");
-      quoteList.innerHTML = quoteOutput;
+      for (var i = 0; i <= quotes.length - 1; i++) {
+        // for (key in quotes[i]) {
+        // console.log(quotes[i]);
+        // if (quotes[i].hasOwnProperty(key)) {
+        quoteBox.innerHTML += quotes[i].quote + "<hr />";
+        authorBox.innerHTML += quotes[i].author + "<hr />";
+        catBox.innerHTML += quotes[i].category + "<hr />";
+      } // hasOwnProperty check
+      // } // for each object
+      // quoteList.innerHTML = quoteOutput;
     });
   });
 });
